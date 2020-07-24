790c1272f50427ba9f639baa8f137a07f17ac357
Miguel Angel Beltrán Rodríguez <68168635+miguelgoz@users.noreply.github.com>
2020-07-24 17:48:47 -0500

    creación del proyecto

---
+++ Source/Simulador/SimuladorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimuladorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMULADOR_API ASimuladorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
