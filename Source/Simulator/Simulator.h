6670438cad77f42d1f71d7673ad5467b5efaba61
Adrian Garcia <adriangarcia@javeriana.edu.co>
2020-10-11 15:05:54 -0500

    all parameters added to Struct S_SimulationConfiguration

--- Source/Simulator/Simulator.h
+++ Source/Simulator/Simulator.h
@@ -23,21 +23,66 @@ struct FS_SimulationConfiguration
 {
 	GENERATED_USTRUCT_BODY();
 
+	//basic parameters
+
 	UPROPERTY(BlueprintReadWrite, Category = Variables)
-		FString SurfaceType;
+		FString Bacteria;
 	UPROPERTY(BlueprintReadWrite, Category = Variables)
-		FString BacteriaType;
+		FString Surface;
 	UPROPERTY(BlueprintReadWrite, Category = Variables)
 		float FactorNutrientUptakePerSize;
 	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		bool DoPresimulation;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		int PresimulationGranularity;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		int BlocksPerSide;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
 		float Temperature;
+
+	//advanced parameters
+
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float InitialEnergyLevel;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float MaximumEnergyLevel;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float NutrientLevelThresholdToMove;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float MinimumEnergyLevel;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float EnergySpentToMove;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float EnergySpentToSurvive;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		int MaximumMovementAttempts;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float MinimumNutrientLevel;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		float UnitsRadiusToMove;
+	UPROPERTY(BlueprintReadWrite, Category = Variables)
+		int InitialNumberObjectPool;
 
 	friend FArchive& operator<<(FArchive& Archive, FS_SimulationConfiguration& Configuration)
 	{
-		Archive << Configuration.SurfaceType;
-		Archive << Configuration.BacteriaType;
+		Archive << Configuration.Bacteria;
+		Archive << Configuration.Surface;		
 		Archive << Configuration.FactorNutrientUptakePerSize;
+		Archive << Configuration.DoPresimulation;
+		Archive << Configuration.PresimulationGranularity;
+		Archive << Configuration.BlocksPerSide;
 		Archive << Configuration.Temperature;
+
+		Archive << Configuration.InitialEnergyLevel;
+		Archive << Configuration.MaximumEnergyLevel;
+		Archive << Configuration.NutrientLevelThresholdToMove;
+		Archive << Configuration.MinimumEnergyLevel;
+		Archive << Configuration.EnergySpentToMove;
+		Archive << Configuration.EnergySpentToSurvive;
+		Archive << Configuration.MaximumMovementAttempts;
+		Archive << Configuration.MinimumNutrientLevel;
+		Archive << Configuration.UnitsRadiusToMove;
+		Archive << Configuration.InitialNumberObjectPool;
 
 		return Archive;
 	}
